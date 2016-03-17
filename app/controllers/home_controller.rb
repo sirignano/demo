4dae8da183aa2562b3d78605e8c25344bc318bdd
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-17 22:12:05 +0100

    autocommit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -33,36 +33,6 @@ class HomeController < ApplicationController
 	end
 
 	def home
-			last = Person.all.order(created_at: :desc)
-			@pp = []
-			k = 0
-			last.each do |o|
-				@pp[k] = []
-				begin
-					per = Finded.find_by! name: o.name
-					@category = o.category
-					@confidence = o.confidence
-					@name = o.name.tr("_", " ").upcase
-					@linkedin = per.linkedin
-					@phone = per.phone
-					@picture = ""
-					@pp[k][0] = 0
-					@pp[k][1] = o.category
-					@pp[k][2] = o.confidence
-					@pp[k][3] = o.name.tr("_", " ").upcase
-					@pp[k][4] = per.linkedin.split('|')[0]
-					@pp[k][5] = per.phone
-					@pp[k][6] = per.linkedin.split('|')[1]
-				rescue
-					@pp[k][0] = 1
-
-				end
-				k += 1
-			end
-			@str = "une erreur non reconnu a été rencontré"
-			@strs = "une personne non reconnu a été rencontré"
-			@nbr = k
-			@a = 1
 	end
 
 	def getinfo
@@ -72,7 +42,6 @@ class HomeController < ApplicationController
 			render1 = ""
 			last.each do |o|
 				@pp[k] = []
-				begin
 					per = Finded.find_by! name: o.name
 					@category = o.category
 					@confidence = o.confidence
@@ -108,27 +77,7 @@ class HomeController < ApplicationController
                                     
                                 </div>
                               </div>'
-				rescue
-					@pp[k][0] = 1
-					render1 += '|'+'<div class="card profile-view">
-                                <div class="pv-header">
-                                    <img src="http://www.babelio.com/users/AVT_Unknown_340.gif" class="pv-main" alt="">
-                                </div>
-
-                                <div class="pv-body">
-                                    <h2>UNKNOWN</h2>
-                                    <small>une personne non reconnu a été rencontré%</small>
-
-                                    <ul class="pv-contact">
-                                    </ul>
-                                    
-                                    <ul class="pv-follow">
-                                    </ul>
-                                    
-                                </div>
-                              </div>'
-
-				end
+				
 				k += 1
 			end
 			@str = "une erreur non reconnu a été rencontré"
