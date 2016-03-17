c2a3e85ce6d90d5ca26332461ae0472d7cdf9548
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-17 14:36:26 +0100

    autocommit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,52 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+a = Finded.new
+a.name = "Alice_Dogaru"
+a.linkedin = "https://www.linkedin.com/in/" + "alice-dogaru-17ab7517"
+a.save
+a = Finded.new
+a.name = "Arthur_Chazal"
+a.linkedin = "https://www.linkedin.com/in/" + "arthur-chazal-63b538b5"
+a.save
+a = Finded.new
+a.name = "Camille_Manano"
+a.linkedin = "https://www.linkedin.com/in/" + "camille-manano-schneider-3984772a"
+a.save
+a = Finded.new
+a.name = "Jean-Christophe_Bertocini"
+a.linkedin = "https://www.linkedin.com/in/" + "jean-christophe-bertoncini-54590060"
+a.save
+a = Finded.new
+a.name = "Julien_Beaumont"
+a.linkedin = "https://www.linkedin.com/in/" + "julienbeaumont"
+a.save
+a = Finded.new
+a.name = "Marie-Ange_Boyomo"
+a.linkedin = "https://www.linkedin.com/in/" + ""
+a.save
+a = Finded.new
+a.name = "Sami_Himeur"
+a.linkedin = "https://www.linkedin.com/in/" + "sami-himeur-1a14b224"
+a.save
+a = Finded.new
+a.name = "Sami_Khalfaoui"
+a.linkedin = "https://www.linkedin.com/in/" + "samikhalfaoui"
+a.save
+a = Finded.new
+a.name = "Simon_Levy"
+a.linkedin = "https://www.linkedin.com/in/" + "silevy"
+a.save
+a = Finded.new
+a.name = "Stephane_Loubere"
+a.linkedin = "https://www.linkedin.com/in/" + "stéphane-loubère-b350212"
+a.save
+a = Finded.new
+a.name = "Romain_Nicot"
+a.linkedin = "https://www.linkedin.com/in/" + "romainnicot"
+a.save
+a = Finded.new
+a.name = "Lucas_Sirignano"
+a.linkedin = "https://www.linkedin.com/in/" + "lucas-sirignano-96730297"
+a.save
