a4d77889b2012aeae2dae63b2f457abd96a11e11
Lucas SIRIGNANO <l.sirignano@groupeonepoint.com>
2016-03-17 15:19:53 +0100

    autocommit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,24 @@
 class HomeController < ApplicationController
 	def home
+		last = Person.all.last
+		per = Finded.find_by! name: last.name
+		@category = last.category
+		@confidence = last.confidence
+		@name = last.name.tr("_", " ").upcase
+		@linkedin = per.linkedin
+		@phone = per.phone
+		@picture = ""
 	end
 	def create
+		rec = params[:data]
+		tab = JSON.parse(rec)
+		tab.each do |t|
+			a = Person.new
+			a.category = t["category"]
+			a.confidence = t["confidence"]
+			a.name = t["name"]
+			a.save
+		end
 	end
 end
+
